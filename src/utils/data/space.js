a55ea8b38d771f5b8d3e337c34d769fe8ac069a9
Luisfer Guevara <luisferguevaraa@gmail.com>
2022-11-11 16:06:44 +0100

    Added Space.js as basic info

---
+++ src/utils/data/space.js
@@ -0,0 +1,108 @@
+export const SPACE = {
+  planets: [
+    {
+      name: "Venus",
+      image:
+        "https://www.nationalgeographic.com.es/medio/2022/07/31/el-planeta-venus_ed75a16a_1200x630.jpg",
+      distance: "255m km",
+      gravity: "3 months",
+      description:"To develop!!!!",
+    },
+    {
+      name: "Mercury",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Mercury_in_true_color.jpg/800px-Mercury_in_true_color.jpg",
+      distance: "2.8 months",
+      gravity: "3.7 m/s2",
+    },
+    {
+      name: "Moon",
+      image: "https://en.wikipedia.org/wiki/Moon#/media/File:FullMoon2010.jpg",
+      distance: "Half a day",
+      gravity: "1.62 m/s2",
+    },
+    {
+      name: "Mars",
+      image:
+        "https://ca.wikipedia.org/wiki/Mart_(planeta)#/media/Fitxer:OSIRIS_Mars_true_color.jpg",
+      distance: "1 week",
+      gravity: "3.72 m/s2",
+    },
+    {
+      name: "Jupiter",
+      image: "https://upload.wikimedia.org/wikipedia/commons/e/e2/Jupiter.jpg",
+      distance: "1 month",
+      gravity: "4.79 m/s2",
+    },
+    {
+      name: "Saturn",
+      image:
+        "https://es.wikipedia.org/wiki/Saturno_(planeta)#/media/Archivo:Saturn_during_Equinox.jpg",
+      distance: "2 months",
+      gravity: "10.44 m/s2",
+    },
+    {
+      name: "Uranus",
+      image:
+        "https://e7.pngegg.com/pngimages/58/47/png-clipart-planet-solar-system-neptune-uranus-knowledge-real-miscellaneous-blue.png",
+      distance: "4 months",
+      gravity: "8.87 m/s2",
+    },
+    {
+      name: "Neptune",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Neptune.jpg/596px-Neptune.jpg",
+      distance: "4 months",
+      gravity: "11.5 m/s2",
+    },
+    {
+      name: "Pluto",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Pluto-01_Stern_03_Pluto_Color_TXT.jpg/1200px-Pluto-01_Stern_03_Pluto_Color_TXT.jpg",
+      distance: "1 year",
+      gravity: "0,62 m/s2",
+    },
+    {
+      name: "Europa",
+      image: "https://upload.wikimedia.org/wikipedia/commons/e/e4/Europa-moon-with-margins.jpg",
+      distance: "2 months",
+      gravity: "1.31 m/s2",
+    },
+    {
+      name: "Titan",
+      image: "https://www.universetoday.com/wp-content/uploads/2016/04/titan_large.jpg",
+      distance: "2 months",
+      gravity: "1.35 m/s2",
+    },
+  ],
+  galaxies: [
+    {
+      name: "Alpha Centauri",
+      image:
+        "https://c4.wallpaperflare.com/wallpaper/597/955/680/sci-fi-galaxy-cosmos-planet-wallpaper-preview.jpg",
+      distance: "2 years sleeping",
+    },
+    {
+      name: "Andromeda",
+      image:
+        "https://besthqwallpapers.com/Uploads/25-11-2017/30404/thumb2-planet-universe-satellite-sci-fi-galaxy.jpg",
+      distance: "6 years sleeping",
+    },
+    {
+      name: "Needle Galaxy",
+      image: "https://i.pinimg.com/originals/e4/d1/46/e4d1467af5b8467333f8658338b25651.jpg",
+      distance: "10 years sleeping",
+    },
+    {
+      name: "Cannis Major",
+      image: "https://cdn.pixabay.com/photo/2019/06/08/05/36/space-4259572_960_720.jpg",
+      distance: "2 years sleeping",
+    },
+    {
+        name: "Black Hole",
+        distance:"No go back",
+        image:"https://cdn.mos.cms.futurecdn.net/4Vv43ekp8QVwL95So7Z8sb.jpg",
+        
+    }
+  ],
+};
