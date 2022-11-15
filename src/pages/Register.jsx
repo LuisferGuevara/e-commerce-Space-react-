8c4d3f8aafe1fd240b52b8642b642464262a6fa5
Luisfer Guevara <luisferguevaraa@gmail.com>
2022-11-15 11:40:43 +0100

    review

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,14 +3,13 @@ import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { newUser } from "../redux/Auth/auth.actions";
-import "../styles/Register.scss"
+import "../styles/Register.scss";
 
 const Register = () => {
-
   const {
     register,
     handleSubmit,
-    formState: { errors, isValid },
+    formState: { errors},
   } = useForm();
 
   const dispatch = useDispatch();
@@ -22,36 +21,35 @@ const Register = () => {
 
   return (
     <div className="register--box">
-
-    <div className="register--container">
-      <h1>Register</h1>
-      <form onSubmit={handleSubmit(registerUser)}>
-        <label>
-          Email
-          <input
-            type="text"
-            {...register("email", {
-              required: "Enter an email",
-              pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i,
-                message: "Thats not a valid email",
-              },
-            })}
-          />
-        </label>
-        {errors.email ? (
-          <>
-            {errors.email.type === "required" && <p>{errors.email.message}</p>}
-            {errors.email.type === "pattern" && <p>{errors.email.message}</p>}
-          </>
-        ) : null}
-        <label>
-          Password
-          <input type="password" {...register("password")} />
-        </label>
-        <button className="glow-on-hover">Send</button>
-      </form>
-    </div>
+      <div className="register--container">
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit(registerUser)}>
+          <label>
+            Email
+            <input
+              type="text"
+              {...register("email", {
+                required: "Enter an email",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i,
+                  message: "Thats not a valid email",
+                },
+              })}
+            />
+          </label>
+          {errors.email ? (
+            <>
+              {errors.email.type === "required" && <p>{errors.email.message}</p>}
+              {errors.email.type === "pattern" && <p>{errors.email.message}</p>}
+            </>
+          ) : null}
+          <label>
+            Password
+            <input type="password" {...register("password")} />
+          </label>
+          <button className="glow-on-hover">Send</button>
+        </form>
+      </div>
     </div>
   );
 };
